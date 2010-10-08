f72b0ca784ce8f7e71f94fa65f1bca3f6bdf2d6b
JericoRevote <JericoRevote@f94e416c-6fcb-4379-9a3f-c32064acf93c>
2010-10-08 04:09:54 +0000

    Changing the spacing.
    git-svn-id: https://www.underworldproject.org/svn/Vrui@35 f94e416c-6fcb-4379-9a3f-c32064acf93c

--- 1.0-068/Vrui/VRWindow.cpp
+++ 1.0-068/Vrui/VRWindow.cpp
@@ -421,7 +421,7 @@ void VRWindow::render(const GLWindow::WindowPos& viewportPos,int screenIndex,con
 		char buffer[20];
 		snprintf(buffer,sizeof(buffer),"%6.1f fps",1.0/vruiState->currentFrameTime);
 		glDisable(GL_LIGHTING);
-		showFpsFont->drawString(GLFont::Vector(showFpsFont->getCharacterWidth()*9.5f,0.0f,0.0f),buffer);
+		showFpsFont->drawString(GLFont::Vector(showFpsFont->getCharacterWidth()*10.0f,0.0f,0.0f),buffer);
 		glEnable(GL_LIGHTING);
 		#endif
 		
@@ -458,7 +458,7 @@ void VRWindow::render(const GLWindow::WindowPos& viewportPos,int screenIndex,con
          snprintf(posBuffer,sizeof(posBuffer),"  Position:%14.8f %14.8f %14.8f\n",pos[0],pos[1],pos[2]);
          snprintf(orientBuffer,sizeof(orientBuffer),"Orientation:%14.8f %14.8f %14.8f %14.8f\n",axis[0],axis[1],axis[2],angle);
          glDisable(GL_LIGHTING);
-	   	showTrackersPosFont->drawString(GLFont::Vector(showTrackersPosFont->getCharacterWidth()*6.0f,75.0f+(deviceIndex*100.0f),0.0f),vruiState->inputDeviceManager->getInputDevice(deviceIndex)->getDeviceName());
+	   	showTrackersPosFont->drawString(GLFont::Vector(showTrackersPosFont->getCharacterWidth()*10.0f,75.0f+(deviceIndex*100.0f),0.0f),vruiState->inputDeviceManager->getInputDevice(deviceIndex)->getDeviceName());
 	   	showTrackersPosFont->drawString(GLFont::Vector(showTrackersPosFont->getCharacterWidth()*60.0f,50.0f+(deviceIndex*100.0f),0.0f),posBuffer);
 	   	showTrackersPosFont->drawString(GLFont::Vector(showTrackersPosFont->getCharacterWidth()*60.0f,25.0f+(deviceIndex*100.0f),0.0f),orientBuffer);
 	      glEnable(GL_LIGHTING);
