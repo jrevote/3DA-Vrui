9c06e012f0323b2ced3bb8dec2b63355f0114475
JericoRevote <JericoRevote@f94e416c-6fcb-4379-9a3f-c32064acf93c>
2012-01-31 04:35:40 +0000

    Updating dataset path.-
    git-svn-id: https://www.underworldproject.org/svn/Vrui@76 f94e416c-6fcb-4379-9a3f-c32064acf93c

--- Scripts/launchEarthModel.sh
+++ Scripts/launchEarthModel.sh
@@ -4,12 +4,12 @@ sudo /sbin/service VRDeviceDaemon stop
 sudo /sbin/service VRDeviceDaemon start
 sleep 20
 
-/usr/local/packages/Vrui-1.0-068/demos/bin/ShowEarthModel \
+/usr/local/packages/Vrui/1.0-068/demos/bin/ShowEarthModel \
 -quakes \
-/usr/local/packages/Vrui-1.0-068/demos/EarthQuakes/1950-2010-Mag6.anss \
-/usr/local/packages/Vrui-1.0-068/demos/EarthQuakes/1950-2010-Mag7.anss \
-/usr/local/packages/Vrui-1.0-068/demos/EarthQuakes/1950-2010-Mag8+.anss \
-/usr/local/packages/Vrui-1.0-068/demos/EarthQuakes/1950-2010-Mag5+.anss \
+/usr/local/packages/Vrui/1.0-068/demos/Datasets/EarthQuakes/1950-2010-Mag6.anss \
+/usr/local/packages/Vrui/1.0-068/demos/Datasets/EarthQuakes/1950-2010-Mag7.anss \
+/usr/local/packages/Vrui/1.0-068/demos/Datasets/EarthQuakes/1950-2010-Mag8+.anss \
+/usr/local/packages/Vrui/1.0-068/demos/Datasets/EarthQuakes/1950-2010-Mag5+.anss \
 -rootSection dante
 
 sudo /sbin/service VRDeviceDaemon stop
