20980c47a64358cae150b91b5a60dd1303bdb067
JericoRevote <JericoRevote@f94e416c-6fcb-4379-9a3f-c32064acf93c>
2012-02-20 05:10:52 +0000

    Explicit restart of VRDeviceDaemon no longer required for each application run.
    git-svn-id: https://www.underworldproject.org/svn/Vrui@80 f94e416c-6fcb-4379-9a3f-c32064acf93c

--- Scripts/launchEarthModel-2.2-003.sh
+++ Scripts/launchEarthModel-2.2-003.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-sudo /sbin/service VRDeviceDaemon-2.2-003 stop
-sudo /sbin/service VRDeviceDaemon-2.2-003 start
-sleep 20
-
 /usr/local/packages/Vrui/2.2-003/demos/bin/ShowEarthModel \
 -quakes \
 /usr/local/packages/Vrui/Datasets/EarthQuakes/1950-2010-Mag6.anss \
@@ -11,5 +7,3 @@ sleep 20
 /usr/local/packages/Vrui/Datasets/EarthQuakes/1950-2010-Mag8+.anss \
 /usr/local/packages/Vrui/Datasets/EarthQuakes/1950-2010-Mag5+.anss \
 -rootSection dante
-
-sudo /sbin/service VRDeviceDaemon-2.2-003 stop
--- Scripts/launchJello-2.2-003.sh
+++ Scripts/launchJello-2.2-003.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-sudo /sbin/service VRDeviceDaemon-2.2-003 stop
-sudo /sbin/service VRDeviceDaemon-2.2-003 start
-sleep 20
-
 /usr/local/packages/Vrui/2.2-003/demos/bin/Jello -rootSection dante
-
-sudo /sbin/service VRDeviceDaemon-2.2-003 stop
