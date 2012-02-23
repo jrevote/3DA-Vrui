f9c1a8e17be4bf36e31d6c3a4d2edd5458a93c77
JericoRevote <JericoRevote@f94e416c-6fcb-4379-9a3f-c32064acf93c>
2012-02-23 04:52:02 +0000

    Rolling back to original default value.
    git-svn-id: https://www.underworldproject.org/svn/Vrui@90 f94e416c-6fcb-4379-9a3f-c32064acf93c

--- 2.2-003/Vrui/Internal/Vrui.General.cpp
+++ 2.2-003/Vrui/Internal/Vrui.General.cpp
@@ -21,7 +21,7 @@ Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 02111-1307 USA
 ***********************************************************************/
 
-#define DELAY_NAVIGATIONTRANSFORMATION 0
+#define DELAY_NAVIGATIONTRANSFORMATION 1
 #define RENDERFRAMETIMES 0
 #define SAVESHAREDVRUISTATE 0
 
