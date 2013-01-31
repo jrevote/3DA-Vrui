29f2d95d290ae1d66ac968825187f5db5de428d0
JericoRevote <JericoRevote@f94e416c-6fcb-4379-9a3f-c32064acf93c>
2013-01-31 23:36:45 +0000

    Updating earthquake path.
    git-svn-id: https://www.underworldproject.org/svn/Vrui@116 f94e416c-6fcb-4379-9a3f-c32064acf93c

--- Scripts/launchEarthModel-2.4-004.sh
+++ Scripts/launchEarthModel-2.4-004.sh
@@ -2,8 +2,8 @@
 
 /usr/local/tools/Vrui/2.4-004/demos/bin/ShowEarthModel \
 -quakes \
-/usr/local/datasets/EarthQuakes/1950-2010-Mag6.anss \
-/usr/local/datasets/EarthQuakes/1950-2010-Mag7.anss \
-/usr/local/datasets/EarthQuakes/1950-2010-Mag8+.anss \
-/usr/local/datasets/EarthQuakes/1950-2010-Mag5+.anss \
+/usr/local/datasets/Earthquakes/1950-2010-Mag6.anss \
+/usr/local/datasets/Earthquakes/1950-2010-Mag7.anss \
+/usr/local/datasets/Earthquakes/1950-2010-Mag8+.anss \
+/usr/local/datasets/Earthquakes/1950-2010-Mag5+.anss \
 -rootSection dante
